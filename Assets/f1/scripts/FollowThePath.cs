8a929edad11e092e72f84fc0db812f59411fd080
stan122334 <stanklimczak1@gmail.com>
2026-08-19 18:35:03 +0100

    Update FollowThePath.cs

--- Assets/f1/scripts/FollowThePath.cs
+++ Assets/f1/scripts/FollowThePath.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class FollowThePath : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] Circle;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int CircleIndex = 0;
+
+    // Use this for initialization
+    private void Start()
     {
-        
+
+        // Set position of Enemy as position of the first waypoint
+        transform.position = Circle[CircleIndex].transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        // Move Enemy
+        Move();
+    }
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+        if (CircleIndex <= Circle.Length - 1)
+        {
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               Circle[CircleIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+            if (transform.position == Circle[CircleIndex].transform.position)
+            {
+                CircleIndex += 1;
+            }
+        }
     }
 }
